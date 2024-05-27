5d19dee4b0388531561d07f59e706bc3fe5a77fc
isfandyar01 <isfandyar0@gmail.com>
2024-05-27 23:26:56 +0500

    will add test

--- Queuew/queue.hpp
+++ Queuew/queue.hpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-enum class queue_status { queue_no_error, queue_full, queue_empty, queue_null };
+enum queue_status { queue_no_error, queue_full, queue_empty, queue_null };
 
 template <typename T> class queue {
 
